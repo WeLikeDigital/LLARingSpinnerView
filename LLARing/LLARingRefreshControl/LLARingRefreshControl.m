8fdbb7674ef065c7cbe04a268593ba7a1a58c979
Ivan Vavilov <vani2@ya.ru>
2014-09-15 12:13:55 +0400

    Add fading to refresh control

--- LLARing/LLARingRefreshControl/LLARingRefreshControl.m
+++ LLARing/LLARingRefreshControl/LLARingRefreshControl.m
@@ -44,11 +44,13 @@ static CGFloat const kPullDistanceDivider = 80;
     if (!self.isRefreshAnimating) {
         CGFloat endAngle = MAX(pullDistance/kPullDistanceDivider * kRefreshControlEndAngle + 3 * M_PI_2, kRefreshControlEndAngle);
         self.spinner.endAngle = endAngle < kRefreshControlEndAngle + 2 * M_PI ? endAngle : kRefreshControlEndAngle;
+        self.spinner.alpha = pullDistance/kPullDistanceDivider;
     }
     
     // If we're refreshing and the animation is not playing, then play the animation
     if (self.isRefreshing && !self.isRefreshAnimating) {
         self.spinner.endAngle = kRefreshControlEndAngle;
+        self.spinner.alpha = 1;
         [self.spinner startAnimating];
         self.isRefreshAnimating = YES;
     }
